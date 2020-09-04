596d9951a2abdb163ba76aff2abedcc2ed1f037e
Braxton Hunter <10922245@my.uvu.edu>
2020-09-03 22:46:44 -0600

    Added Player Points Script

---
+++ Cyborg Shrimp/Assets/Scripts/PlayerPoints.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPoints : MonoBehaviour
+{
+    public string pointsTotal = "500";
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    
+}
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

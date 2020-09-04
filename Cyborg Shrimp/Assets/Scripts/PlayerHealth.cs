b9544fc9d2cbc0c7e710d280b4137e1e22886e90
Braxton Hunter <10922245@my.uvu.edu>
2020-09-03 22:46:11 -0600

    Updated Player Health Script

--- Cyborg Shrimp/Assets/Scripts/PlayerHealth.cs
+++ Cyborg Shrimp/Assets/Scripts/PlayerHealth.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerHealth : MonoBehaviour
 {
+    public float health = 100f;
     // Start is called before the first frame update
     void Start()
     {
-        
+         
     }
 
     // Update is called once per frame

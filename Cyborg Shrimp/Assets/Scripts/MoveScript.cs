b2fa16dabf387c1ebc6f82614ff8186499f41d07
Braxton Hunter <10922245@my.uvu.edu>
2020-09-05 23:30:47 -0600

    Updated Mover Script

--- Cyborg Shrimp/Assets/Scripts/MoveScript.cs
+++ Cyborg Shrimp/Assets/Scripts/MoveScript.cs
@@ -9,14 +9,13 @@ public class MoveScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log(message: "Hello World!");
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        x = speed*Input.GetAxis("Horizontal") * Time.deltaTime;
+        x = speed * Input.GetAxis("Horizontal") * Time.deltaTime;
         y = speed * Input.GetAxis("Vertical") * Time.deltaTime;
         transform.Translate(x, y, z);
     }

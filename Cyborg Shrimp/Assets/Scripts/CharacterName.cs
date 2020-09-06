43fb241f4da6f2929f5449cd5ca839d8be2d00af
Braxton Hunter <10922245@my.uvu.edu>
2020-09-05 23:30:36 -0600

    Added Character Name Script

---
+++ Cyborg Shrimp/Assets/Scripts/CharacterName.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterName : MonoBehaviour
+{
+    public string nameOfCharacter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hello! Your name is" + nameOfCharacter + "Press Start to Begin!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

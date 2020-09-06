f525f5d6b5ba8d92572d57b05f56cb7a57d1b307
Braxton Hunter <10922245@my.uvu.edu>
2020-09-05 23:31:00 -0600

    Added Start Script

--- Cyborg Shrimp/Assets/Scripts/StartScript.cs
+++ Cyborg Shrimp/Assets/Scripts/StartScript.cs
@@ -2,8 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterMover : MonoBehaviour
+public class StartScript : MonoBehaviour
 {
+    
     // Start is called before the first frame update
     void Start()
     {

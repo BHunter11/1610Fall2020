d5e9f8624bae550d27227c5b34456955558e9b5a
Braxton Hunter <10922245@my.uvu.edu>
2020-09-10 23:53:24 -0600

    Added Fixed Health Power Up Script (Test)

--- Cyborg Shrimp/Assets/Scripts/PowerUp.cs
+++ Cyborg Shrimp/Assets/Scripts/PowerUp.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HealthPowerUpScript : MonoBehaviour
+public class PowerUp : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()

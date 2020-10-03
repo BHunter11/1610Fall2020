f358e79ab77fa5a6f47067a1e0e72837fc10777a
Braxton Hunter <10922245@my.uvu.edu>
2020-10-03 00:05:15 -0600

    Create with Code cont.

--- Create With Code/Prototype1/Assets/Scripts/PlayerController.cs
+++ Create With Code/Prototype1/Assets/Scripts/PlayerController.cs
@@ -11,17 +11,17 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    public float speed = 20;
-    public float turnSpeed;
-    public float horizontalInput;
-    public float forwardInput;
+    private float speed = 20.0f;
+    private float turnSpeed =45.0f;
+    private float horizontalInput;
+    private float forwardInput;
         void Update()
     {
-        // Move the vehicle forward
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
-        
+        // Moves the car forward based on vertical input
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        //rotates the car based on horizontal input
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
     }
 }

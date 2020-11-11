24b4ae898e77b2594440ec3cb0d0d1499dc01889
Braxton Hunter <10922245@my.uvu.edu>
2020-11-10 19:29:27 -0700

    Homework Catchup

--- Prototype 4/Assets/Challenge 4/Scripts/EnemyX.cs
+++ Prototype 4/Assets/Challenge 4/Scripts/EnemyX.cs
@@ -11,7 +11,8 @@ public class EnemyX : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        enemyRb = GetComponent<Rigidbody>();
+        enemyRb = GetComponent<Rigidbody>(); 
+        playerGoal = GameObject.Find("Player Goal");
     }
 
     // Update is called once per frame
@@ -20,7 +21,7 @@ public class EnemyX : MonoBehaviour
         // Set enemy direction towards player goal and move there
         Vector3 lookDirection = (playerGoal.transform.position - transform.position).normalized;
         enemyRb.AddForce(lookDirection * speed * Time.deltaTime);
-
+        
     }
 
     private void OnCollisionEnter(Collision other)
---
+++ Prototype 5/Assets/Course Library/Scripts/GameManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    private float spawnRate = 1.0f;
+    private int score;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+        isGameActive = true;
+    }
+
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
